6e6ad1e3047bfcdd8b88a047a3ddf5f9b4d71dc0
Rupal Gupta <63002494+rupalg@users.noreply.github.com>
2020-11-20 05:20:22 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -33,6 +33,43 @@ upcoming_events = [
 	"location": "Flushing Meadows Park"
 	}
 ]
+
+people = [
+	{
+	"id": 1,
+	"name": "Harry",
+	"date": "Monday, November 16",
+	"time": "7PM",
+	"location": "Library Hall",
+	"language": "French",
+	},
+	{
+	"id": 2,
+	"name": "Hermionie",
+	"date": "Friday, November 20",
+	"time": "11AM",
+	"location": "Local Restaurant",
+	"language": "Mandarin",
+	},
+	{
+	"id": 3,
+	"name": "Ron",
+	"date": "Wednesday, November 24",
+	"time": "7PM",
+	"location": "Library Room 14",
+	"language": "Spanish",
+	},
+	{
+	"id": 4,
+	"name": "Edward",
+	"date": "Wednesday, November 24",
+	"time": "8PM",
+	"location": "Flushing Meadows Park",
+	"language": "Urdu",
+	}
+]
+
+
 @app.route('/')
 def home(name=None):
 	return render_template('home.html')
@@ -40,6 +77,10 @@ def home(name=None):
 @app.route('/events')
 def search(name=None):
 	return render_template('events.html', upcoming_events=upcoming_events)
+
+@app.route('/event_selected')
+def event_selected(name=None):
+	return render_template('event_selected.html', upcoming_events=upcoming_events, people=people)
 
 if __name__ == '__main__':
    app.run(debug = True)
