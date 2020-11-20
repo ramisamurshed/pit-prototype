64555264d69acddbbdb1340577967e369e51cf81
Ramisa Murshed <35822743+ramisamurshed@users.noreply.github.com>
2020-11-20 00:43:13 -0500

    fixed card error

--- static/events.js
+++ static/events.js
@@ -2,21 +2,34 @@ function create_events() {
 
 	var current_id = 1
 
-	for (var i = 0; i < 4; i++) {
-		var upcoming_event
+	//for (var i = 0; i < 4; i++) {
 
-		var card = $("<div>")
-		card.addClass("card")
+		//var card = $("<div>")
+		//card.addClass("card")
 
-		var card_body = $("<div>")
-		card_body.addClass("card-body")
+		//var card_body = $("<div>")
+		//card_body.addClass("card-body")
+
+		var back_button = $("<button type='button'>Back to Home</button>")
+		back_button.addClass("btn")
+		back_button.addClass("btn-secondary")
+		$('#back-to-home').append(back_button)
+
+		back_button.click(function(){
+			window.location.href = "/"
+		})
 
 		$.each(upcoming_events, function(index, data) {
 			if(data["id"] == current_id) {
 				upcoming_event = data
 				current_id++
 			}
-		})
+		
+		var card = $("<div>")
+		card.addClass("card")
+
+		var card_body = $("<div>")
+		card_body.addClass("card-body")
 
 		var card_title = $("<h4 class='card-title'>")
 		card_title.append(upcoming_event["name"])
@@ -43,7 +56,8 @@ function create_events() {
 		card.append(card_body)
 
 		$("#upcoming-events").append(card)
-	}
+		})
+	//}
 }
 
 $(document).ready(function(){
