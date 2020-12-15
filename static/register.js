ba1006e86605991952af92c91f36505befdb66c3
Ramisa Murshed <35822743+ramisamurshed@users.noreply.github.com>
2020-12-15 14:03:52 -0500

    Update register.js

--- static/register.js
+++ static/register.js
@@ -1,3 +1,13 @@
+function log_in() {
+	var login_button = $('<button type="button" id="loginbtn">Back to Home</button>')
+
+	login_button.click(function(){
+		window.location.href = "/"
+	})
+
+	$("#log-in").append(login_button)
+}
+
 $(document).ready(function(){
-	
+	log_in()
 })
